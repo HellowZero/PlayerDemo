6588729238f38ad9b29c2e6d842beae9bdb8f3ae
云祥 王 <w990650947@outlook.com>
2023-11-11 22:17:59 +0800

    添加项目文件。

---
+++ PlayerDemo/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using FlyleafLib;
+
+namespace PlayerDemo;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        // Ensures that we have enough worker threads to avoid the UI from freezing or not updating on time
+        ThreadPool.GetMinThreads(out int workers, out int ports);
+        ThreadPool.SetMinThreads(workers + 6, ports + 6);
+        EngineConfig engineConfig;
+        engineConfig = DefaultEngineConfig();
+        Engine.StartAsync(engineConfig);
+    }
+    private EngineConfig DefaultEngineConfig()
+    {
+        EngineConfig engineConfig = new EngineConfig();
+
+        engineConfig.PluginsPath = ":Plugins";
+        engineConfig.FFmpegPath = ":FFmpeg";
+        engineConfig.FFmpegHLSLiveSeek
+                                    = true;
+        engineConfig.UIRefresh = true;
+        engineConfig.FFmpegDevices = true;
+
+#if RELEASE
+            engineConfig.LogOutput      = "Flyleaf.FirstRun.log";
+            engineConfig.LogLevel       = LogLevel.Debug;
+#else
+        engineConfig.LogOutput = ":debug";
+        engineConfig.LogLevel = LogLevel.Debug;
+        engineConfig.FFmpegLogLevel = FFmpegLogLevel.Warning;
+#endif
+
+        return engineConfig;
+    }
+}
---
+++ PlayerDemo/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using FlyleafLib.Controls.WPF;
+using MahApps.Metro.Controls;
+
+namespace PlayerDemo;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainViewModel mainViewModel
+    {
+        get; set;
+    }
+    public MainWindow()
+    {
+        mainViewModel = new MainViewModel(this);
+        InitializeComponent();
+        
+        this.DataContext = mainViewModel;
+      
+    }
+
+}
